802a97af3ccb308e0a09d024bb89e5e722d549ae
Ekaterina Davydova <ekaterina.davydova@student.hamk.fi>
2024-09-10 10:04:00 +0300

    The sub and add functions are added
    The choice of subtraction and sum is added for the user input

--- src/App.java
+++ src/App.java
@@ -18,12 +18,27 @@ public class App {
         System.out.println("Please give the value of the 2st number ");
         answer2 = in.nextLine();
 
-        int result = 0;
-        result = Integer.parseInt(answer1) + Integer.parseInt(answer2);
-            if (result > 10)
+        String answer3 = "";
+        System.out.println("Would yu like to calculate the sum or substraction. Please type plus or minus");
+        answer3 = in.nextLine();
+        int sub = Integer.parseInt(answer1) - Integer.parseInt(answer2);
+        int add = Integer.parseInt(answer1) + Integer.parseInt(answer2);
+            if (answer3.equals("plus") || answer3.equals("Plus"))
             {
-                System.out.println("The value is greater than 10. It is " + result);
+                System.out.println("The result is " + add);
             }
+
+            else if (answer3.equals("minus") || answer3.equals("Minus"))
+            {
+                System.out.println("The result is " + sub);
+            }
+
+        // int result = 0;
+        // result = Integer.parseInt(answer1) + Integer.parseInt(answer2);
+        //     if (result > 10)
+        //     {
+        //         System.out.println("The value is greater than 10. It is " + result);
+        //     }
 
     }
 }
